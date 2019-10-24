f04d1975bdcb74275ebda3dad1f7d4c6200bae22
daveckw <daveckw@gmail.com>
2019-10-24 22:08:26 +0800

    Updates

---
+++ precache-manifest.4bed25876f01e40336e0fe6dad53adef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52fe4c5229a75eda2d834228039c4b6",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "590acce81a545bd315af",
+    "url": "/crown-clothing/static/css/main.9411132e.chunk.css"
+  },
+  {
+    "revision": "aa923db2fa95a1b189c3",
+    "url": "/crown-clothing/static/js/2.40d9634e.chunk.js"
+  },
+  {
+    "revision": "590acce81a545bd315af",
+    "url": "/crown-clothing/static/js/main.a80a2c9b.chunk.js"
+  },
+  {
+    "revision": "33a0f66328f8eaa9fce9",
+    "url": "/crown-clothing/static/js/runtime-main.19baa7bd.js"
+  }
+]);
